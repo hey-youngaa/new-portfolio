ae557df07a43b8a17da4ae3d95f3b6f3d8d95857
Jessica Choi <jessicahchoi93@gmail.com>
2023-07-28 20:37:27 -0400

    folders for home,work and about page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Work from './pages/work/Work'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />} />
+        <Route path="/work" element={<Work />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
