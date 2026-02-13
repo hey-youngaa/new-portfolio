7680813a8aaa0ed278442d24e172fe04f3c5f4dd
Jessica Choi <jessicahchoi93@gmail.com>
2026-02-12 20:32:40 -0500

    fixed harris teeter card

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './home.css'
 import choco from '../../assets/images/choco.png'
-import june from '../../assets/images/juneWork.jpg'
+import june from '../../assets/images/juneWork.jpeg'
 import { Link } from 'react-router-dom'
 import homeCover from '../../assets/images/homeDecor.jpg'
-import codibook from '../../assets/images/codibook.jpg'
+import ht from '../../assets/images/ht.jpg'
 import arrow from '../../assets/images/arrow-up-right.svg'
 
 function Home() {
@@ -17,11 +17,25 @@ function Home() {
 
       <h2>Recent Works</h2>
       <div className="workSection">
+
+        <div className="block">
+          <img src={ht} alt=''  className='workCover'/>
+          <div className="workDetails">
+            <h3>Harris Teeter Mobile App Redesign</h3>
+            <p>Improving how shoppers discover deals and navigate grocery shopping</p>
+            <Link to={'/harris-teeter-redesign'} className='workLink'>
+              <div className="workLinkBox">
+                <img src={arrow} alt='link arrow' />
+                <div className="workLinkText">View Project</div>
+              </div>
+            </Link>
+          </div>
+        </div>
         <div className="block">
           <img src={june} alt="" className='workCover'/>
           <div className="workDetails">
             <h3>JUNE</h3>
-            <p>Portfolio design and development for the Providence-based artist.</p>
+            <p>Portfolio design and development for the Providence-based artist</p>
             <a href='https://elephantinthesnake.com/' rel='noreferrer' target='_blank' className='workLink'>
               <div className="workLinkBox">
                 <img src={arrow} alt='link arrow' />
@@ -37,20 +51,6 @@ function Home() {
             <h3>The Cozy Space</h3>
             <p>App design and prototype for a home furniture & decor concept</p>
             <Link to={'/home-shopping-concept'} className='workLink'>
-              <div className="workLinkBox">
-                <img src={arrow} alt='link arrow' />
-                <div className="workLinkText">View Project</div>
-              </div>
-            </Link>
-          </div>
-        </div>
-
-        <div className="block">
-          <img src={codibook} alt="" className='workCover'/>
-          <div className="workDetails">
-            <h3>Codibook</h3>
-            <p>PDP design of a korean fashion eccomerce website</p>
-            <Link to={'/codibook-PDP'} className='workLink'>
               <div className="workLinkBox">
                 <img src={arrow} alt='link arrow' />
                 <div className="workLinkText">View Project</div>
