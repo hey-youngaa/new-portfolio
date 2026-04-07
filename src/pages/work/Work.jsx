269a3636471569e1efe6ee1803bb9756b087f303
Jessica Choi <jessicahchoi93@gmail.com>
2026-04-07 01:53:12 -0400

    removed gallery and added june work

--- src/pages/work/Work.jsx
+++ src/pages/work/Work.jsx
@@ -1,46 +1,71 @@
 import React from 'react'
-import Masonry,{ResponsiveMasonry} from 'react-responsive-masonry'
+
 import './work.css'
 import { projects } from '../../assets/data/Data'
-import { blenderWork } from '../../assets/data/Data'
+import june from '../../assets/images/juneWork.jpeg'
+
 import { Link } from 'react-router-dom'
 
 function Work() {
   return (
-    <div className="pageContainer">
-      <a href='https://elephantinthesnake.com/' rel='noreferrer' target='_blank' className='case-links'>
-          <div className="work">
-            <h2>
-            <span className='caseNumber'> 01. </span>
-            <span className='caseTitle'>June</span>
-            </h2>
-            <p className="tag">Website Design & Development</p>
-          </div>
-        </a>
+    <div className="workContainer">
+      <h1>Work</h1>
 
+      <div className="caseStudies">
         {projects.map((item) => {
           return(
-            <Link to={`/${item.id}`} className={item.cName}>
-              <div className="work">
+            <div className="caseStudy">
+              <Link to={`/${item.id}`} className={item.cName}>
                 <h2>
-                  <span className='caseNumber'> 0{item.number}.</span> 
-                  <span className="workTitle"> {item.title}</span>
+                  <span className="caseNumber"> 0{item.number}. </span>
+                  <span className="caseTitle">{item.title}</span>
                 </h2>
-                <p className='tag'>{item.tags}</p>
-              </div>
-            </Link>
+              </Link>
+              <p className='tag'>{item.tags}</p>
+            </div>
           )
         })}
-        <br/>
+      </div>
+
+      <div className="creativeSection">
+        <h2>Creative Explorations</h2>
+
+          <div className="creativeItem">
+            <div className="imageWrapper">
+              <a href='https://elephantinthesnake.com/' target='_blank' rel='noreferrer'>
+                <img src={june} alt='artist portfolio website i developed' />
+              </a>
+            </div>
+            <p className="creativeTitle">Artist Portfolio Website</p>
+            <p className='creativeDesc'>
+              Designed and developed to showcase artist's creative work
+            </p>
+          </div>
+
+{/* WILL FIGURE OUT HOW TO LAYOUT THIS SECTION LATER
         <ResponsiveMasonry columnsCountBreakPoints={{350:1, 750:2, 900:3}}>
-          <Masonry gutter='2em'>
+          <Masonry gutter='12px'>
+            
+            <div className="creativeItem webDevItem">
+              <img src={june}></img>
+              <p>Web design and development artist portfolio</p>
+              <p>Click <a href='https://elephantinthesnake.com/' rel='noreferrer' target='_blank'>here</a> to view</p>
+            </div>  
+
+            
             {blenderWork.map((item) => {
               return(
-                <img src={item.image} alt={item.alt} loading='lazy'/>
+                <div className="creativeItem blenderItem">
+                  <img src={item.image} alt={item.alt} loading='lazy'/>
+                </div>
               )
             })}
           </Masonry>
         </ResponsiveMasonry>
+*/}
+      </div>
+
+
     </div>
   )
 }
