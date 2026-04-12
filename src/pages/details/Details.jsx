0290e26c752cc91599cf0c3d2e58cb1da6fce357
Jessica Choi <jessicahchoi93@gmail.com>
2026-04-11 22:51:26 -0400

    Added back to work button at end of case studies

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,5 +1,5 @@
-import React,{useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom'
+import React, {useState, useEffect} from 'react'
+import { Link, useParams } from 'react-router-dom'
 import { projects } from '../../assets/data/Data'
 import OneCol from '../../components/onecol/OneCol'
 
@@ -16,11 +16,18 @@ function Details() {
   
 
   return (
-    <div style={{backgroundColor: '#f7f7f7'}}>
+    <div style={{backgroundColor: '#f7f7f7', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px 0'}}>
         {study ? (
             <OneCol gallery={study.images} />
         ) : null}
 
+        <Link to="/work" className='backBtn'
+            style={{display: 'inline-block', backgroundColor: '#c4dd92', borderRadius: '40% 35% 55% 45% / 55% 55% 40% 45%'
+                , padding: '12px 20px', textDecoration: 'none', fontWeight: 'bold', 
+            }}
+        >
+            Back to Work
+        </Link>
     </div>
   )
 }
